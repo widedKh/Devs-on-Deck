7d0ed1d6e0a2fba899777c1b16c88b8d70744c3e
widedKh <khediriwided@gmail.com>
2023-11-16 18:34:16 +0100

    16-11-2023

--- src/main/java/com/w/DevsOnDeck/controllers/PositionController.java
+++ src/main/java/com/w/DevsOnDeck/controllers/PositionController.java
@@ -20,126 +20,116 @@ import com.w.DevsOnDeck.models.Position;
 import com.w.DevsOnDeck.services.OrgService;
 import com.w.DevsOnDeck.services.PositionService;
 
-
-
 @Controller
 public class PositionController {
-	
-	@Autowired
-	PositionService positionService;
-	
-	
-	@Autowired
-	OrgService orgService;
-	
-	// ------ READ ALL
-	@GetMapping("/orgs/dashboard")
-	public String positionsList(@ModelAttribute("position") Position position, Model model, HttpSession session) {
-	    Long userId = (Long) session.getAttribute("organization_id");
-	    if (userId == null) {
-	        return "redirect:/";
-	    } else {
-	        Organization orgs = orgService.findOrg(userId);
-	        List<Position> positions = positionService.allPositions();
-	        model.addAttribute("org",orgs );
-	        model.addAttribute("positions",positions);
-	        return "dashboard.jsp";
-	    }
-	}
 
-	
-   //display form
-    @GetMapping("/orgs/job/new")
-	public String addPosition(@ModelAttribute("position") Position position, Model model) {
-		return "addPosition.jsp";
+    @Autowired
+    PositionService positionService;
+
+    @Autowired
+    OrgService orgService;
+
+    // ------ READ ALL
+    @GetMapping("/orgs/dashboard")
+    public String positionsList(@ModelAttribute("position") Position position, Model model, HttpSession session) {
+	Long userId = (Long) session.getAttribute("organization_id");
+	if (userId == null) {
+	    return "redirect:/";
+	} else {
+	    Organization orgs = orgService.findOrg(userId);
+	    List<Position> positions = positionService.allPositions();
+	    model.addAttribute("org", orgs);
+	    model.addAttribute("positions", positions);
+	    return "dashboard.jsp";
 	}
-	
-    
-    //add a new position
+    }
+
+    // display form
+    @GetMapping("/orgs/job/new")
+    public String addPosition(@ModelAttribute("position") Position position, Model model) {
+	return "addPosition.jsp";
+    }
+
+    // add a new position
 
     @PostMapping("/orgs/job/new")
-    public String createTeam(@Valid @ModelAttribute("position") Position position, BindingResult result, HttpSession session) {
-        Long userId = (Long) session.getAttribute("organization_id");
+    public String createTeam(@Valid @ModelAttribute("position") Position position, BindingResult result,
+	    HttpSession session) {
+	Long userId = (Long) session.getAttribute("organization_id");
 
-        if (userId == null) {
-            return "redirect:/"; 
-        }
+	if (userId == null) {
+	    return "redirect:/orgs/login";
+	}
 
-        if (result.hasErrors()) {
-            return "addPosition.jsp";
-        } else {
-           Organization org = orgService.findOrg(userId);
-            position.setOrganization(org);
-            positionService.createPosition(position);
-            return "redirect:/orgs/dashboard";
-        }
+	if (result.hasErrors()) {
+	    return "addPosition.jsp";
+	} else {
+	    Organization org = orgService.findOrg(userId);
+	    position.setOrganization(org);
+	    positionService.createPosition(position);
+	    return "redirect:/orgs/dashboard";
+	}
     }
 
-    
-    //display one team
+    // display one team
     @GetMapping("/orgs/job/{id}")
-    public String showPosition(@PathVariable("id") Long id, Model model,HttpSession session) {
-    	 Long userId = (Long) session.getAttribute("organization_id");
-    	 if(userId == null) {
-			 return "redirect:/";
-		 }
-    	else {
-         	Organization org = orgService.findOrg(userId);
-    		Position position = positionService.findPosition(id);
-        	model.addAttribute("position", position);
-        	model.addAttribute("org",org);
-        	
-    	 return "showPosition.jsp";
+    public String showPosition(@PathVariable("id") Long id, Model model, HttpSession session) {
+	Long userId = (Long) session.getAttribute("organization_id");
+	if (userId == null) {
+	    return "redirect:/";
+	} else {
+	    Organization org = orgService.findOrg(userId);
+	    Position position = positionService.findPosition(id);
+	    model.addAttribute("position", position);
+	    model.addAttribute("org", org);
+
+	    return "showPosition.jsp";
 	}
-    } 
-    
-  //edit a position
+    }
+
+    // get the edit form
     @GetMapping("/orgs/{id}/edit")
     public String editPosition(Model model, @PathVariable("id") Long id, HttpSession session) {
-    	 Long userId = (Long) session.getAttribute("organization_id");
-    	if(userId == null) {
-    		return "redirect:/orgs/login";
-    	}
-    	Position position = positionService.findPosition(id);
-    	model.addAttribute("position", position);
-    	model.addAttribute("org", orgService.findOrg(userId));
-    	
-    	return "editPosition.jsp";
+	Long userId = (Long) session.getAttribute("organization_id");
+	if (userId == null) {
+	    return "redirect:/orgs/login";
+	}
+	Position position = positionService.findPosition(id);
+	model.addAttribute("position", position);
+	model.addAttribute("org", orgService.findOrg(userId));
+
+	return "editPosition.jsp";
     }
     
+    //edit position
     @PutMapping("/orgs/{id}/edit")
-    public String updatePosition(Model model, @Valid @ModelAttribute("position") Position position, BindingResult result, HttpSession session) {
-    	 Long userId = (Long) session.getAttribute("user_id");
-    	if(userId == null) {
-    		return "redirect:/orgs/login";
-    	}
-    	
-    	if(result.hasErrors()) {
-    		return "editPosition.jsp";
-    	}
-    	Organization org = orgService.findOrg(userId);
-    	position.setOrganization(org);
-    	positionService.updatePosition(position);
-    	
-    	return "redirect:/orgs/dashboard";
+    public String updatePosition(Model model, @Valid @ModelAttribute("position") Position position,
+	    BindingResult result, HttpSession session) {
+	Long userId = (Long) session.getAttribute("user_id");
+	if (userId == null) {
+	    return "redirect:/orgs/login";
+	}
+
+	if (result.hasErrors()) {
+	    return "editPosition.jsp";
+	}
+	Organization org = orgService.findOrg(userId);
+	position.setOrganization(org);
+	positionService.updatePosition(position);
+
+	return "redirect:/orgs/dashboard";
     }
 
-
-
-
-    //delete a team 
+    // delete a position
     @GetMapping("/orgs/delete/{id}")
     public String removePosition(@PathVariable("id") Long id, HttpSession session) {
-        Long userId = (Long) session.getAttribute("organization_id");
-        if (userId == null) {
-            return "redirect:/";
-        }
-        else {
-            positionService.deletePosition(id);
-        return "redirect:/orgs/dashboard";
-    }
-    }
-    
-
+	Long userId = (Long) session.getAttribute("organization_id");
+	if (userId == null) {
+	    return "redirect:/orgs/login";
+	} else {
+	    positionService.deletePosition(id);
+	    return "redirect:/orgs/dashboard";
+	}
     }
 
+}
